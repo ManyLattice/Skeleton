4426f87a3c7a655c86621e248858afc9ce50785d
Mikhail <manylattice7742@yandex.kz>
2021-09-14 12:46:18 +0600

    Update GradientView.swift

--- Skeleton/GradientView.swift
+++ Skeleton/GradientView.swift
@@ -11,31 +11,27 @@ import Cocoa
 /// A view who's `layerClass` is a `CAGradientLayer`.
 class GradientView: NSView {
     
-    
-    override var layer: CALayer? {
-        get {
-            let gl = CAGradientLayer()
-            gl.frame = .init(origin: .zero, size: .init(width: 100, height: 100))
-            
-            return gl
-        }
-        set {}
+    override func makeBackingLayer() -> CALayer {
+        let layer = CAGradientLayer()
+        layer.needsDisplayOnBoundsChange = true
+        return layer
     }
+    
 }
 
 extension GradientView {
-  /// A convenient way to access the `GradientView`'s corresponding `CAGradientLayer`.
-  var gradientLayer: CAGradientLayer {
-    return layer as! CAGradientLayer
-  }
-  
-  /// A convenient way to slide the `GradientView`'s corresponding `CAGradientLayer`.
-  func slide(to dir: Direction, group: ((CAAnimationGroup) -> Void) = { _ in }) {
-    return gradientLayer.slide(to: dir, group: group)
-  }
-  
-  /// A convenient way to stop sliding the `GradientView`'s corresponding `CAGradientLayer`.
-  func stopSliding() {
-    return gradientLayer.stopSliding()
-  }
+    /// A convenient way to access the `GradientView`'s corresponding `CAGradientLayer`.
+    var gradientLayer: CAGradientLayer {
+        return layer as! CAGradientLayer
+    }
+    
+    /// A convenient way to slide the `GradientView`'s corresponding `CAGradientLayer`.
+    func slide(to dir: Direction, group: ((CAAnimationGroup) -> Void) = { _ in }) {
+        return gradientLayer.slide(to: dir, group: group)
+    }
+    
+    /// A convenient way to stop sliding the `GradientView`'s corresponding `CAGradientLayer`.
+    func stopSliding() {
+        return gradientLayer.stopSliding()
+    }
 }
